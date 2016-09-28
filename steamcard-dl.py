7b32d5acc4d6911a7f4b0221e705d21e6d215e7f
Crystalight <werevv@gmail.com>
2016-09-28 03:03:48 +0300

    Added logging to file Other changes: * every image extension is now detected by its content * Unicode support is added whenever possible * exceptions are handled better now (there is still an unexplained logging issue) * complete list of titles to parse is selected by default starting with this version * minor output changes and bug-fixes

--- steamcard-dl.py
+++ steamcard-dl.py
@@ -3,48 +3,41 @@
 import urllib2
 import sys
 from bs4 import BeautifulSoup
-import imghdr
 import os
+import logging
+from PIL import Image
+import time
 
+RETRY_TIMES = 10
+
+# Cookie handler
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-try:
-    os.mkdir("data")
-    os.chdir("data")
-except Exception as e:
-    os.chdir("data")
+
 
 def adjustFileNameToWinOS(filename):
-    deletechars = u'\/:*?"<>|*'
+    deletechars = u'\/:*?"<>|\t'
     for c in deletechars:
         filename = filename.replace(c,u'')
     return filename
 
 def downloadImage(gameName, imageName, url, fType):
-    isBg = False
-
-    #imageName = imageName[:imageName.find("-")-1]
-
-    #file_name = url.split('/')[-1]
     if fType == "card":
         imageName = imageName[:imageName.find("- Series")-1]
-        file_name = u"{0} [Cards] - {1}{2}".format(gameName, imageName.lstrip(), url[url.rfind("."):])
-        #i = 0
-        #while os.path.isfile(file_name):
-        #    i += 1
-        #    file_name = u"{0} [Cards] - {1} [{2}]{3}".format(gameName, imageName.lstrip(), i, url[url.rfind("."):])
-
-    else: # fType == bg
+        file_name = u"{0} [Cards] - {1}".format(gameName, imageName.lstrip())
+    else:  # fType == bg
         imageName = imageName[:imageName.find("- Type:")-1]
         file_name = u"{0} [Backgrounds] - {1}".format(gameName, imageName.lstrip())
-        isBg = True
 
     file_name = adjustFileNameToWinOS(file_name)
 
-    u = opener.open(url)
+    for i in range(RETRY_TIMES):
+        try:
+            u = opener.open(url)
+            break
+        except (urllib2.HTTPError,urllib2.URLError) as e:
+            logging.debug(u"Address from %s was not available when tried to download. Error: %s", gameName, e)
+
     f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading:   %-100s ||| Size: %7s bytes" % (file_name, file_size)
 
     file_size_dl = 0
     block_sz = 8192
@@ -56,54 +49,66 @@ def downloadImage(gameName, imageName, url, fType):
 
         file_size_dl += len(buffer)
         f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        #print status,
 
     f.close()
 
-    if isBg:
-        bgFileType = imghdr.what(file_name)
-        i = 0
-        file_name_new = file_name
-        while os.path.isfile(file_name_new + u".{0}".format(bgFileType)):
-            i += 1
-            file_name_new = file_name + u" [{0}]".format(i)
-        os.rename(file_name, file_name_new + u".{0}".format(bgFileType))
+    imgFileType = Image.open(file_name).format.lower()
+    i = 0
+    file_name_new = file_name
+    while os.path.isfile(file_name_new + u".{0}".format(imgFileType)):
+        i += 1
+        file_name_new = file_name + u" [{0}]".format(i)
+    file_name_new = file_name_new + u".{0}".format(imgFileType)
+    os.rename(file_name, file_name_new)
+
+    print u"Downloaded:   %-100s ||| Size: %7s bytes" % (file_name_new, os.stat(file_name_new).st_size)
 
 
 def main():
+    try:
+        os.mkdir("data")
+        os.chdir("data")
+    except Exception as e:
+        os.chdir("data")
+
+    logging.basicConfig(filename='Errors.log',format='%(asctime)s | %(levelname)s | %(message)s',level=logging.DEBUG)
+
     BASEURL = "http://www.steamcardexchange.net/index.php?showcase"
 
     categoryURLList = ["-filter-ac", "-filter-df", "-filter-gi", "-filter-jl", "-filter-mo", "-filter-pr", "-filter-su", "-filter-vx", "-filter-yz", "-filter-09"]
     gamesDict = {}
 
     for filterURL in categoryURLList:
-        print "Grabbing {}".format(BASEURL + filterURL)
-        page = opener.open(BASEURL + filterURL)
+        print u"Grabbing {}".format(BASEURL + filterURL)
+        try:
+            page = opener.open(BASEURL + filterURL)
+        except (urllib2.HTTPError,urllib2.URLError) as e:
+            logging.debug(u'Grabbing %s failed. Error: %s', filterURL, e)
+            continue
+
         soup = BeautifulSoup(page.read(),"html.parser")
 
         games = soup.findAll("div", attrs={"class":"showcase-game-item"})
 
         for game in games:
             gameLink = game.select("a")[0]
-
             name = gameLink.find("img").get("alt")
-
-            # print gameLink.get("href")
-            # print name
-
             gamesDict[name] = gameLink.get("href")
 
     temp = sorted(gamesDict.keys())
-    start_index = temp.index('Deponia')
-    cropped_list = temp[start_index:]
+    start_index = 0             #temp.index('God Mode')
+    finish_index = len(temp)    #temp.index('TurnOn')
+    cropped_list = temp[start_index:finish_index]
     for key in cropped_list:
-        # print "{0}: {1}".format(key.encode("utf-8"), gamesDict[key].encode("utf-8"))
-        page = opener.open("http://www.steamcardexchange.net/" + gamesDict[key])
+        for i in range(RETRY_TIMES):
+            try:
+                page = opener.open("http://www.steamcardexchange.net/" + gamesDict[key])
+            except (urllib2.HTTPError,urllib2.URLError) as e:
+                logging.debug(u'Game "%s" page is not available. Error: %s', key, e)
+                time.sleep(1)
+
         soup = BeautifulSoup(page.read(),"html.parser")
 
-        # TODO: specify only "TRADING CARDS" section not to do the same work twice ("FOILS" have same artworks)
         cards = soup.findAll("a", {"rel":"lightbox-normal"})
 
         hdBgrounds = soup.findAll("a", {"rel":"lightbox-background"})
@@ -116,30 +121,38 @@ def main():
                 hdImageLink = card.get("href")
                 hdImageName = card.get("title")
                 if not hdImageLink in alreadyDoneCardLinks:
-                    downloadImage(key, hdImageName, hdImageLink, "card")
+                    for i in range(RETRY_TIMES):
+                        try:
+                            downloadImage(key, hdImageName, hdImageLink, "card")
+                            break
+                        except (urllib2.HTTPError,urllib2.URLError) as e:
+                            logging.debug(u'Address of "%s" is not available. Error: %s', hdImageName, e)
+                            time.sleep(1)
                     alreadyDoneCardLinks.append(hdImageLink)
-                    # print "hdImageLink = {0}".format(hdImageLink)
-                    # print "hdImageName = {0}".format(hdImageName)
             except Exception as e:
-                print "Card ERROR: {0}".format(str(e))
-                print key + " - " + hdImageName
-                if str(e) == "[Errno 28] No space left on device":
+                print u"Card ERROR: {0}".format(str(e))
+                logging.debug(u'Another weird error occurred for "%s". Error: %s', key, e)
+                if str(e) == u"[Errno 28] No space left on device":
                     sys.exit(1)
-                continue
 
         for hdBg in hdBgrounds:
             try:
                 hdBgLink = hdBg.get("href")
                 hdBgName = hdBg.get("title")
                 if not hdBgLink in alreadyDoneBgLinks:
-                    downloadImage(key, hdBgName, hdBgLink, "bg")
+                    for i in range(RETRY_TIMES):
+                        try:
+                            downloadImage(key, hdBgName, hdBgLink, "bg")
+                            break
+                        except (urllib2.HTTPError,urllib2.URLError) as e:
+                            logging.debug(u'Address of "%s" is not available. Error: %s', hdBgName, e)
+                            time.sleep(1)
                     alreadyDoneBgLinks.append(hdBgLink)
             except Exception as e:
-                print "Background ERROR: {0}".format(str(e))
-                print key + " - " + hdBgName
-                if str(e) == "[Errno 28] No space left on device":
+                print u"Background ERROR: {0}".format(str(e))
+                logging.debug(u'Another weird error occurred for "%s". Error: %s', key, e)
+                if str(e) == u"[Errno 28] No space left on device":
                     sys.exit(1)
-                continue
 
 if __name__ == "__main__":
     try:
